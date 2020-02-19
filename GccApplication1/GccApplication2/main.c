95a6420c8be035ce32765b44223ff585c6f39d3b
Marleen <mroubos2@avans.nl>
2020-02-19 12:20:50 +0100

    Track #2 leeg

---
+++ GccApplication1/GccApplication2/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication2.c
+ *
+ * Created: 19-2-2020 12:17:49
+ * Author : marle
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
